24cda44b6495451c88fcecdde20b55d64d9fb191
Rachel-wanjau <rachel.wanjau@gmail.com>
2023-04-24 10:31:21 -0700

    Two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+  *main - Prints two digit numbers 00 to 99 separated by a , in ascending order
+  *
+  *Return: Always 0.
+  */
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = 0; n < 10; n++)
+	{
+		for (m = 0; m < 10; m++)
+		{
+		putchar((n % 10) + '0');
+		putchar((m % 10) + '0');
+
+		if (n == 9 && m == 9)
+			continue;
+
+		putchar(',');
+		putchar(',');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
