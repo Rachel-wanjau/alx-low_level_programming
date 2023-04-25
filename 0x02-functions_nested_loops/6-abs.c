f1646e12b20e9b1f3be311debd197f9a0111ff4a
Rachel-wanjau <rachel.wanjau@gmail.com>
2023-04-25 14:00:52 -0700

    Sign

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ *
+ * @r: input number as an integer
+ *
+ * Return: the absolute value of int
+ */
+int _abs(int r)
+{
+	if (r >= 0)
+	{
+		return (r);
+	}
+	else
+	{
+		return (r * -1);
+	}
+}
