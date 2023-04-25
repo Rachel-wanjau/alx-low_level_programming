2cf04616fa3e3be8a2ef6ec4981c8e7ea4abf54d
Rachel-wanjau <rachel.wanjau@gmail.com>
2023-04-25 13:14:53 -0700

    Alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet 10 times in lowercase,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	int a;
+	int c;
+
+	for (c = a; a <= 10; a++)
+	{
+		for (c = 97; c <= 122; a++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
